4434b005e93bf4a4703e26095fb65e763cff74f0
angel <aortiz@itexico.com>
2020-02-03 19:19:46 -0600

    Separating TopDownRoom from the abstract Room class.

--- CoreEngine/src/main/java/com/github/zerokode/objects/room/Room.java
+++ CoreEngine/src/main/java/com/github/zerokode/objects/room/Room.java
@@ -1,19 +1,35 @@
 package com.github.zerokode.objects.room;
 
+import com.github.zerokode.exceptions.LayerNotFoundException;
 import com.github.zerokode.objects.room.layers.Layer;
+import lombok.Getter;
+
+import java.util.List;
 
 /**
- * A room seen in top-down perspective. The list of layers are predefined for this type of room, that way we can
- * implement some the possible interactions between the elements present in each layer.
+ * A room has multiple layers, each one containing multiple objects.
+ * <p>
+ * The layer with the lower index zero will be the background while layers with a greater index
+ * will be closer to the foreground.
  */
-public class Room {
+@Getter
+public abstract class Room {
 
-    private Layer floor;
-    private Layer floorMarks;
-    private Layer walls;
-    private Layer roomObjects;
-    private Layer characters;
-    private Layer particles;
-    private Layer sky;
+    private List<Layer> layers;
+
+    public Room(List<Layer> layers) {
+        this.layers = layers;
+    }
+
+    /**
+     * Finds a layer by name.
+     * @param layerName - case sensitive
+     * @return the layer that has the given name.
+     * @throws LayerNotFoundException when none has been found.
+     */
+    public Layer getByName(String layerName) throws LayerNotFoundException {
+        return layers.stream().filter(l -> l.getLayerName().equals(layerName)).findFirst()
+                .orElseThrow(() -> new LayerNotFoundException(layerName));
+    }
 
 }
--- CoreEngine/src/main/java/com/github/zerokode/objects/room/layers/Layer.java
+++ CoreEngine/src/main/java/com/github/zerokode/objects/room/layers/Layer.java
@@ -1,9 +1,15 @@
 package com.github.zerokode.objects.room.layers;
 
+import lombok.Getter;
+
+@Getter
 public class Layer extends Grid {
 
-    public Layer(int cellsWide, int cellsTall) {
+    private String layerName;
+
+    public Layer(String layerName, int cellsWide, int cellsTall) {
         super(cellsWide, cellsTall);
+        this.layerName = layerName;
     }
 
 }
